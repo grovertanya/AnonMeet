94af8d9a2675b10af1aafa2de813d919b94c7069
nkhur <145411496+nkhur@users.noreply.github.com>
2025-11-02 17:52:36 -0500

    fixed useState, useEffect

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useState, useEffect } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { InterviewRoom } from './components/InterviewRoom';
