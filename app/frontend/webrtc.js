77e71229691453cf7444d4592779f86a37917fb0
nkhur <145411496+nkhur@users.noreply.github.com>
2025-11-02 12:53:41 -0500

    render pt. 2

--- app/frontend/webrtc.js
+++ app/frontend/webrtc.js
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io({
-  path: "/api/socket",
-});
+const socket = io('https://anon-meet-backend.onrender.com');
 
 socket.emit("join-room", "room-123");
 socket.on("signal", (data) => console.log("Got signal:", data));
